e7f52a33ab2123279f07fb92d1c6b74d802918bb
Rafael Setragni <rafaelsetra@gmail.com>
2019-12-27 18:14:55 -0300

    Splitting responsive project from JSX

--- lib/flutter_responsive.dart
+++ lib/flutter_responsive.dart
@@ -1,15 +1,6 @@
 library flutter_responsive;
 
-export 'src/typography/responsive_text.dart';
-export 'src/typography/responsive_typography.dart';
-export 'src/typography/responsive_stylesheet.dart';
-
 export 'src/grid/responsive_row.dart';
 export 'src/grid/responsive_col.dart';
 export 'src/grid/responsive_screen.dart';
 export 'src/grid/responsive_container.dart';
-
-export 'src/parser/jsx_parser.dart';
-export 'src/parser/jsx_node.dart';
-export 'src/parser/jsx_node_text.dart';
-export 'src/parser/jsx_node_element.dart';
